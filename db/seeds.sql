393d822d8f2774ec5b0af253eebc412cf938896d
Maira Acevedo <m.acevedo53@yahoo.com>
2021-01-18 15:39:48 -0800

    updated views and public folders

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Plant-based Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hot Cheeto Burger', FALSE);
