d1d9e7534d589010b6b0a39ae4185901c4503542
Maira Acevedo <m.acevedo53@yahoo.com>
2021-02-08 22:13:49 -0800

    fixed api routes

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -9,34 +9,24 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 
-router.get("/", function(req, res){
-    res.redirect("/burgers");
-});
+router.get('/', function(req, res){
+  burger.all(function(burger_data){
+    // console.log(burger_data);
+    res.render('index', {burger_data});
+  })
+})
 
-router.get("/burgers", function(req, res) {
-  burger.all(function(burgerData) {
-      res.render("index", {burger_data: burgerData});
-    
+router.put('/burgers/update', function(req, res){
+  burger.update(req.body.burger_id, function(result){
+    // console.log(result);
+    res.redirect('/');
   });
-
 });
 
-router.post("/burgers/create", function(req, res) {
-  burger.create(req.body.burger_name, function(result) {
-    // Send back the ID of the new quote
-    console.log(result);
-    res.redirect("index", {burger_data: burgerData});
-  });
+router.post('/burgers/create', function(req, res){
+  burger.create(req.body.burger_name, function(result){
+    res.redirect('/');
+  })
+})
 
-});
-
-router.put("/burgers/id:", function(req, res) {
-    burger.update(req, params.id, function (result){
-        console.log(result);
-        res.sendStatus(200);
-    });
-
-});
-
-// Export routes for server.js to use.
 module.exports = router;
