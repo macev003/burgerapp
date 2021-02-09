74c0dfd5c8d26cb270c39868c26ec4ebc3239e2a
Maira Acevedo <m.acevedo53@yahoo.com>
2021-02-08 23:05:21 -0800

    more route fixes

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,17 +1,36 @@
-  $(document).ready(function () {
-
-    $(".devour-form").on("submit", function (event) {
-        event.preventDefault();
-
-        var burger_id = $(this).children(".burger_id").val();
-        console.log(burger_id);
-        $.ajax({
-            method: "PUT",
-            url: "/burgers/" + burger_id
-        }).then(function (data) {
-            // reload page to display devoured burger in proper column
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+    
+        var newState = {
+          devoured: true
+        };
+    
+        
+        $.ajax("/api/burger/" + id, {
+          type: "PUT",
+          data: newState
+        }).then(
+          function () {
+            console.log("changed devoured to true");
+            
             location.reload();
-        });
+          }
+        );
+      });
+    $(".burgerInput").on("submit", function(event) {
+    event.preventDefault();
+    var newBurger = {
+        burger_name: $("#newBurger").val().trim()
+    };
+    
+    $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+    }).then(function() {
+        console.log("New Burger Created");
+        location.reload();
+    })
+    })
 
-    });
-});  
+})
